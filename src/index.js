3e65dce35b5792507db04370477029957aaa06b0
da <>
2018-09-13 00:27:29 +0300

    Implement calculation zeros in factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+  let numberOf2 = 0;
+  let numberOf5 = 0;
+
+  for(let i = 1; i <= number; i++) {
+    let test = i
+
+    while(test % 2 === 0) {
+      numberOf2++;
+      test /= 2;
+    }
+
+    while(test % 5 === 0) {
+      numberOf5++;
+      test /= 5;
+    }
+  }
+
+  return numberOf2 > numberOf5 ? numberOf5 : numberOf2;
 }
