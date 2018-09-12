e3242b56c8e4aa83b16376368c00126939f44efd
da <>
2018-09-13 02:18:46 +0300

    Fix issue with performance

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
+const cache = [];
+
 module.exports = function getZerosCount(number) {
 
   let numberOf2 = 0;
   let numberOf5 = 0;
 
-  for(let i = 1; i <= number; i++) {
+  let j = 1;
+
+  if(cache.length > 0) {
+    const cachedItem = cache.find(el => el.num < number);
+
+    if(cachedItem) {
+      j = cachedItem.num + 1;
+      numberOf2 = cachedItem.numberOf2;
+      numberOf5 = cachedItem.numberOf5;
+    }
+  }
+
+  for(let i = j; i <= number; ++i) {
     let test = i
 
-    while(test % 2 === 0) {
-      numberOf2++;
-      test /= 2;
-    }
-
     while(test % 5 === 0) {
-      numberOf5++;
+      ++numberOf5;
       test /= 5;
     }
   }
 
-  return numberOf2 > numberOf5 ? numberOf5 : numberOf2;
+  cache.push({num: number, numberOf2:numberOf2, numberOf5:numberOf5});
+
+  cache.sort((a, b) => { return b.num - a.num});
+
+  return numberOf5;
 }
